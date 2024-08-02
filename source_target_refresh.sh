aeb8ea0ad982123958ca79b178f19413e1b17097
Seamus Lee <seamuslee001@gmail.com>
2024-08-02 10:08:08 +1000

    Make improvements to script and config by removing specific references to DRUPAL DB in variable names and fixing an issue with Drupal dump file not being correct for linux and also adding wp search replace if we are doing wordpress

--- source_target_refresh.sh
+++ source_target_refresh.sh
@@ -30,13 +30,13 @@ echo $password | sudo -S chown www-data:www-editors -R $TARGET_DIR;
 get_source_dump() {
   cd $SOURCE_DIR
   if [ $CMS = 'Drupal' ]; then
-    vendor/bin/drush sql:dump --result-file=~/source_drupal.sql
+    vendor/bin/drush sql:dump --result-file=~/source_$CMS.sql
   elif [ $CMS = 'Wordpress' ]; then
     wp db export ~/source_$CMS.sql
   fi
 
 
-  mysqldump -u $SOURCE_MYSQL_USER -p$SOURCE_MYSQL_PASS -h $SOURCE_DB_ARGS --add-drop-table $SOURCE_CIVI_DB > ~/source_civicrm.sql
+  mysqldump -u $SOURCE_MYSQL_USER -p$SOURCE_MYSQL_PASS $SOURCE_DB_ARGS --add-drop-table $SOURCE_CIVI_DB > ~/source_civicrm.sql
   sed -i 's/DEFINER=[^*]*\*/\*/g' ~/source_civicrm.sql
   sed -i 's/DEFINER=[^*]*\*/\*/g' ~/source_$CMS.sql
 }
@@ -47,9 +47,9 @@ copy_source_to_target() {
 
 create_target_db() {
   echo "Create drupal database for target site";
-  db_exists=`$TARGET_MYSQLCMD -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$TARGET_DRUPAL_DB'" | wc -c`
+  db_exists=`$TARGET_MYSQLCMD -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$TARGET_CMS_DB'" | wc -c`
   if [ $db_exists = 0 ]; then
-    echo $password | sudo -S mysql -e "CREATE DATABASE $TARGET_DRUPAL_DB";
+    echo $password | sudo -S mysql -e "CREATE DATABASE $TARGET_CMS_DB";
   fi
   db_exists=`$TARGET_MYSQLCMD -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$TARGET_CIVI_DB'" | wc -c`
   if [ $db_exists = 0 ]; then
@@ -58,15 +58,19 @@ create_target_db() {
   fi
 
   echo "Create mysql user for drupal database";
-  echo $password | sudo -S mysql -e "GRANT ALL PRIVILEGES ON $TARGET_DRUPAL_DB.* TO '$TARGET_MYSQL_USER'@'localhost' IDENTIFIED BY '$TARGET_MYSQL_PASS';FLUSH PRIVILEGES;";
+  echo $password | sudo -S mysql -e "GRANT ALL PRIVILEGES ON $TARGET_CMS_DB.* TO '$TARGET_MYSQL_USER'@'localhost' IDENTIFIED BY '$TARGET_MYSQL_PASS';FLUSH PRIVILEGES;";
   echo "Create mysql user for civicrm database";
   echo $password | sudo -S mysql -e "GRANT ALL PRIVILEGES ON $TARGET_CIVI_DB.* TO '$TARGET_MYSQL_USER'@'localhost' IDENTIFIED BY '$TARGET_MYSQL_PASS';FLUSH PRIVILEGES;";
 }
 
 dump_target_db() {
   echo "Dumping target databases with source mysqldumps";
-  $TARGET_MYSQLCMD $TARGET_DRUPAL_DB < ~/source_$CMS.sql
+  $TARGET_MYSQLCMD $TARGET_CMS_DB < ~/source_$CMS.sql
   $TARGET_MYSQLCMD $TARGET_CIVI_DB < ~/source_civicrm.sql
+  if [ $CMS = 'Wordpress' ]; then
+    cd $TARGET_DIR
+    wp search-replace --all-tables-with-prefix $SOURCE_CMS_URL $TARGET_CMS_URL
+  fi
   echo "Remove source site mysqldumps"
   echo $password | sudo -S rm ~/source_$CMS.sql
   echo $password | sudo -S rm ~/source_civicrm.sql
